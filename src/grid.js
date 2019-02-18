e7ea9660958d2b22b4b6487bd0e25a05697c5f2e
ALEJANDRO <alejandro.ginesmartinez@gmail.com>
2019-02-18 08:16:17 +0100

    testing Radium

--- src/grid.js
+++ src/grid.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import breakpoint from './breakpoints';
 import { PropTypes } from 'prop-types';
+import StyleRoot from 'radium';
 
 const grid = (props) => {
 
@@ -135,7 +136,7 @@ col.propTypes = {
     children: PropTypes.node
 }
 
-const Grid = Radium(grid);
+const Grid = StyleRoot(Radium(grid));
 const Row = Radium(row);
 const Col = Radium(col);
 
