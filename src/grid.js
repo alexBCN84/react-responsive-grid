41dca3fc22bfd9b2ca28c75479a6d9dfaf10f6a1
ALEJANDRO <alejandro.ginesmartinez@gmail.com>
2019-02-24 16:03:05 +0100

    removed radium, media queries implemented in js with window.matchMedia API

--- src/grid.js
+++ src/grid.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import breakpoint from './breakpoints';
+import React, {Component} from 'react';
 import { PropTypes } from 'prop-types';
-import Radium, {StyleRoot} from 'radium';
 
 const grid = (props) => {
-
+    
     const gridStyles = {
         position: "relative",
         width: "100%",
         maxWidth: ((props.fullWidth && "100%") || (props.maxWidth && props.maxWidth)) || "1200px",
         margin: "0 auto"
     };
-
+    const gutters = (props.gutters === 0 && 1) || (props.gutters || 32);
     const { children } = props;
     const rows = React.Children.map(children, child =>
-        React.cloneElement(child, { gutters: props.gutters, reverse: props.reverse })
+        React.cloneElement(child, { gutters: gutters, reverse: props.reverse })
     );
     return (
-        <StyleRoot><div style={{ ...gridStyles, ...props.style }}>{rows}</div></StyleRoot>
+       <div style={{ ...gridStyles, ...props.style }}>{rows}</div>
     )
 }
 
@@ -39,18 +37,25 @@ const row = (props) => {
                     : "center"
     );
     const componentStyles = {
-        paddingLeft: props.gutters ? (props.gutters / 2) : 16,
-        paddingRight: props.gutters ? (props.gutters / 2) : 16,
+        paddingLeft: props.gutters / 2,
+        paddingRight: props.gutters / 2,
         display: "flex",
         flexWrap: "wrap",
         justifyContent: columnsAlignment,
         flexFlow: props.reverse ? "row-reverse wrap" : "wrap"
     }
 
-    const { children } = props;
 
+    const { children } = props;
+    const mqls = [
+        window.matchMedia("(min-width: 0px)"),
+        window.matchMedia("(min-width: 480px)"),
+        window.matchMedia("(min-width: 768px)"),
+        window.matchMedia("(min-width: 1024px)"),
+        window.matchMedia("(min-width: 1280px)")
+    ]
     const columns = React.Children.map(children, child =>
-        React.cloneElement(child, { gutters: props.gutters, totalCols: props.totalCols || 12 })
+        React.cloneElement(child, { mqls: mqls, gutters: props.gutters, totalCols: props.totalCols || 12 })
     );
 
     return <div style={{ ...componentStyles, ...props.style }}>{columns}</div>;
@@ -62,60 +67,73 @@ row.propTypes = {
     reverse: PropTypes.bool,
     children: PropTypes.node.isRequired
 }
-
-const col = (props) => {
-    const spanning = (nOfCols = 0, totalCols, offset, gutters) => {
-        if (nOfCols === 0) return 0;
-        if (nOfCols < 0 || nOfCols % 1 !== 0) return (function () { throw `${nOfCols} is not a valid number for this row` }());
-        if (nOfCols > totalCols) return (function () { throw `the maximum number of columns for this row is ${totalCols}` }());
-        const width = nOfCols === undefined ? null : ({ width: `calc(100% / 12 * ${nOfCols} - ${gutters || 32}px)` }) 
-        const marginLeft = offset === undefined ? null : ({ marginLeft:`calc(100% / 12 * ${offset} - ${gutters || 32}px)` });
-        return {
-            display: nOfCols === 0 ? 'none' : 'block',
-            ...width,
-            ...marginLeft
-        };
-    };
-
-    const { width, mobile, phablet, tablet, desktop, wideDesktop, totalCols } = props;
-    const { offset, mobileOffset, phabletOffset, tabletOffset, desktopOffset, wideDesktopOffset } = props;
-
-    if (
-        width === undefined &&
-        mobile === undefined &&
-        phablet === undefined &&
-        tablet === undefined &&
-        desktop === undefined &&
-        wideDesktop === undefined
-    ) return (function () { throw `Use at least one of the following props to assign column span: width, mobile, phablet, tablet, desktop, wideDesktop.` }())
-
-    const columnStyles = {
+class Column extends Component {
+    state = {
         position: "relative",
-        margin: props.gutters === 0 ? 0 : (props.gutters / 2) || 16,
-        ...spanning(width, totalCols, offset, props.gutters),
-        [breakpoint.mobile]: {
-            ...spanning(mobile, totalCols, mobileOffset, props.gutters)
-        },
-        [breakpoint.phablet]: {
-            ...spanning(phablet, totalCols, phabletOffset, props.gutters)
-        },
-        [breakpoint.tablet]: {
-            ...spanning(tablet, totalCols, tabletOffset, props.gutters)
-        },
-        [breakpoint.desktop]: {
-            ...spanning(desktop, totalCols, desktopOffset, props.gutters)
-        },
-        [breakpoint.wideDesktop]: {
-            ...spanning(wideDesktop, totalCols, wideDesktopOffset, props.gutters)
+        margin: this.props.gutters / 2
+    }
+    componentDidMount(){
+        for (var i=0; i<this.props.mqls.length; i++){
+            this.mediaqueryresponse(this.props.mqls[i]) // call listener function explicitly at run time
+            this.props.mqls[i].addListener(this.mediaqueryresponse) // attach listener function to listen in on state changes
         }
     }
 
-    return (
-        <div style={{ ...columnStyles, ...props.style }}>{props.children}</div>
-    )
-};
+    mediaqueryresponse = (mql) => {
+        const { width, mobile, phablet, tablet, desktop, wideDesktop, totalCols } = this.props;
+        const { offset, mobileOffset, phabletOffset, tabletOffset, desktopOffset, wideDesktopOffset } = this.props;
+        const spanning = (nOfCols = 0, totalCols, offset, gutters) => {
+            if (nOfCols === 0) return 0;
+            if (nOfCols < 0 || nOfCols % 1 !== 0) return (function () { throw `${nOfCols} is not a valid number for this row` }());
+            if (nOfCols > totalCols) return (function () { throw `the maximum number of columns for this row is ${totalCols}` }());
+            const width = nOfCols === undefined ? null : ({ width: `calc(100% / 12 * ${nOfCols} - ${gutters}px)`}) 
+            const marginLeft = offset === undefined ? null : ({ marginLeft:`calc(100% / 12 * ${offset} - ${gutters}px)`});
+            return {
+                display: nOfCols === 0 ? 'none' : 'block',
+                ...width,
+                ...marginLeft
+            };
+        };
+        if (
+            width === undefined &&
+            mobile === undefined &&
+            phablet === undefined &&
+            tablet === undefined &&
+            desktop === undefined &&
+            wideDesktop === undefined
+        ) return (function () { throw `Use at least one of the following props to assign column span: width, mobile, phablet, tablet, desktop, wideDesktop.` }())
+    
+            
+        if ((width || mobile) && this.props.mqls[0].matches){ // {min-width: 0px} query matched
+            this.setState({...this.state, ...spanning(mobile || width, totalCols, offset, this.props.gutters)});
+        }
+        if (phablet && this.props.mqls[1].matches){ // {min-width: 480px} query matched
+            this.setState({...this.state, ...spanning(phablet, totalCols, offset, this.props.gutters)});
+        }
+        if (tablet && this.props.mqls[2].matches){ // {min-width: 768px} query matched
+            this.setState({...this.state, ...spanning(tablet, totalCols, offset, this.props.gutters)});
+        }
+        if (desktop && this.props.mqls[3].matches){ // {min-width: 1024px} query matched
+            this.setState({...this.state, ...spanning(desktop, totalCols, offset, this.props.gutters)});
+        }
+        if (wideDesktop && this.props.mqls[4].matches){ // {min-width: 1280px} query matched
+            this.setState({...this.state, ...spanning(wideDesktop, totalCols, offset, this.props.gutters)});
+        }
+        console.log(this.state)
+    }
+    componentWillUnmount() {
+        for (var i=0; i<this.props.mqls.length; i++){
+            this.props.mqls[i].removeListener(this.mediaqueryresponse) // remove listener function on state changes
+        }
+        
+    }
+    render() {
+        return <div style={{...this.state, ...this.props.style }}>{this.props.children}</div>
+    }
+}
 
-col.propTypes = {
+
+Column.propTypes = {
     styles: PropTypes.object,
     gutters: PropTypes.number,
     width: PropTypes.number,
@@ -134,9 +152,9 @@ col.propTypes = {
     children: PropTypes.node
 }
 
-const Grid = Radium(grid);
-const Row = Radium(row);
-const Col = Radium(col);
+const Grid = grid;
+const Row = row;
+const Col = Column;
 
 export { Grid, Row, Col };
 
